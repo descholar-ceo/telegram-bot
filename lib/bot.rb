aa1795883471b122b9ef35d65224d2131676e47e
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-29 17:29:26 +0200

    Fix half of rubocop errors

--- lib/bot.rb
+++ lib/bot.rb
@@ -8,33 +8,39 @@ class Bot
   def initialize
     Telegram::Bot::Client.run(Configs::BASE_API_KEY) do |bot|
       bot.listen do |message|
-        case message.text.downcase
-        when 'start'
-          bot.api.send_message(chat_id: message.chat.id, text: Messages::WELCOME_MSG)
-        when 'help'
-          bot.api.send_message(chat_id: message.chat.id, text: Messages::HELP_MESSAGE)
-        when 'quote'
-          quote_obj = LookUp.new(Configs::PROGRAMMING_QUOTE_API)
-          my_quote = quote_obj.read
-          bot.api.send_message(chat_id: message.chat.id, text: Formatter.format_response('quote', my_quote).to_s)
-        when 'word'
-          quote_obj = LookUp.new(Configs::WORD_OF_DAY_API)
-          my_word = quote_obj.read
-          bot.api.send_message(chat_id: message.chat.id, text: Formatter.format_response('word', my_word).to_s)
-        when 'time'
-          bot.api.send_message(
-            chat_id: message.chat.id,
-            text: Formatter.format_response('time', nil, message.from.first_name, message.from.last_name)
-          )
-        when 'date'
-          bot.api.send_message(
-            chat_id: message.chat.id,
-            text: Formatter.format_response('date', nil, message.from.first_name, message.from.last_name)
-          )
-        else
-          bot.api.send_message(chat_id: message.chat.id, text: Messages::BAD_COMMAND)
-        end
+        cases(message, bot)
       end
+    end
+  end
+
+  private
+
+  def cases(message, bot)
+    case message.text.downcase
+    when 'start'
+      bot.api.send_message(chat_id: message.chat.id, text: Messages::WELCOME_MSG)
+    when 'help'
+      bot.api.send_message(chat_id: message.chat.id, text: Messages::HELP_MESSAGE)
+    when 'quote'
+      quote_obj = LookUp.new(Configs::PROGRAMMING_QUOTE_API)
+      my_quote = quote_obj.read
+      bot.api.send_message(chat_id: message.chat.id, text: Formatter.format_response('quote', my_quote).to_s)
+    when 'word'
+      quote_obj = LookUp.new(Configs::WORD_OF_DAY_API)
+      my_word = quote_obj.read
+      bot.api.send_message(chat_id: message.chat.id, text: Formatter.format_response('word', my_word).to_s)
+    when 'time'
+      bot.api.send_message(
+        chat_id: message.chat.id,
+        text: Formatter.format_response('time', nil, message.from.first_name, message.from.last_name)
+      )
+    when 'date'
+      bot.api.send_message(
+        chat_id: message.chat.id,
+        text: Formatter.format_response('date', nil, message.from.first_name, message.from.last_name)
+      )
+    else
+      bot.api.send_message(chat_id: message.chat.id, text: Messages::BAD_COMMAND)
     end
   end
 end
--- lib/formatter.rb
+++ lib/formatter.rb
@@ -1,15 +1,28 @@
+# Formatter is in charge of all formatting reponses to user
 class Formatter
   def self.format_response(res_type, res_object = nil, fname = nil, lname = nil)
-    current_date_time = Time.new
+    message_response(res_type, res_object)
+    date_time(res_type, fname, lname)
+  end
+
+  private
+
+  def message_response(res_type, res_object)
     case res_type
     when 'quote'
       "Today\'s Programming quote is:\n\n#{res_object['en']}\n\nAuthor:\t#{res_object['author']}"
     when 'word'
       "Today\'s word of bravity is:\n\n#{res_object['text']}\n\nAuthor:\t#{res_object['author']}"
-    when 'time'
-      "Hello #{fname} #{lname},\nThe current time is #{current_date_time.strftime('%I:%M %p')}"
+    end
+  end
+
+  def date_time(res_type, fname = nil, lname = nil)
+    current_date_time = Time.new
+    case res_type
     when 'date'
       "Hello #{fname} #{lname},\nThe current date is #{current_date_time.strftime('%a, %B %d, %Y')}"
+    when 'time'
+      "Hello #{fname} #{lname},\nThe current time is #{current_date_time.strftime('%I:%M %p')}"
     end
   end
 end
