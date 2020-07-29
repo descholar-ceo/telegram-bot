0b1e5c6411e3f7c5981ef83fd265bf1dcdaf063a
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-29 15:37:10 +0200

    Fix rubocop lint error

--- lib/messages.rb
+++ lib/messages.rb
@@ -1,5 +1,5 @@
 module Messages
-    WELCOME_MSG='Hello! From nezabot'
-    HELP_MESSAGE='That is the list of help'
-    BAD_COMMAND='Sorry! Invalid input, type help and hit send for more info'
+  WELCOME_MSG = 'Hello! From nezabot'.freeze
+  HELP_MESSAGE = 'That is the list of help'.freeze
+  BAD_COMMAND = 'Sorry! Invalid input, type help and hit send for more info'.freeze
 end
