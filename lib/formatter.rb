8b491fbb3b86cad06169ea69a63b410d05be4a7c
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-29 20:54:51 +0200

    Fix typo mistake in formatter and delete comments

--- lib/formatter.rb
+++ lib/formatter.rb
@@ -1,8 +1,6 @@
 # Formatter is in charge of all formatting reponses to user
 class Formatter
   def self.format_response(res_type, res_object = nil, fname = nil, lname = nil)
-    # message_response(res_type, res_object)
-    # date_time(res_type, fname, lname)
     case res_type
     when 'quote'
       "Today\'s Programming quote is:\n\n#{res_object['en']}\n\nAuthor:\t#{res_object['author']}"
