f925579dfbee394e9e8981e7a79c196308bd0b6a
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-29 15:29:51 +0200

    Fix case faulty

--- lib/bot.rb
+++ lib/bot.rb
@@ -23,11 +23,13 @@ class Bot
           my_word = quote_obj.read
           bot.api.send_message(chat_id: message.chat.id, text: Formatter.format_response('word', my_word).to_s)
         when 'time'
-          current_date_time = Time.new
-          bot.api.send_message(chat_id: message.chat.id, text: "Hello #{message.from.first_name} #{message.from.last_name},\nThe current time is #{current_date_time.strftime('%I:%M %p')}")
+          bot.api.send_message(
+            chat_id: message.chat.id,
+            text: Formatter.format_response('time', nil, message.from.first_name, message.from.last_name))
         when 'date'
-          current_date_time = Time.new
-          bot.api.send_message(chat_id: message.chat.id, text: "Hello #{message.from.first_name} #{message.from.last_name},\nThe current date is #{current_date_time.strftime('%a, %B %d, %Y')}")
+          bot.api.send_message(
+            chat_id: message.chat.id,
+            text: Formatter.format_response('date', nil, message.from.first_name, message.from.last_name))
         else
           bot.api.send_message(chat_id: message.chat.id, text: 'Sorry! Invalid input, type help and hit send for more info')
         end
--- lib/formatter.rb
+++ lib/formatter.rb
@@ -1,10 +1,15 @@
 class Formatter
-  def self.format_response(res_type, res_object)
+  def self.format_response(res_type, res_object = nil, fname = nil, lname = nil)
+    current_date_time = Time.new
     case res_type
     when 'quote'
       "Today\'s Programming quote is:\n\n#{res_object['en']}\n\nAuthor:\t#{res_object['author']}"
     when 'word'
       "Today\'s word of bravity is:\n\n#{res_object['text']}\n\nAuthor:\t#{res_object['author']}"
+    when 'time'
+        "Hello #{fname} #{lname},\nThe current time is #{current_date_time.strftime('%I:%M %p')}"
+    when 'date'
+        "Hello #{fname} #{lname},\nThe current date is #{current_date_time.strftime('%a, %B %d, %Y')}"
     end
   end
 end
