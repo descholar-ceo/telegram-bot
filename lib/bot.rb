5a12b9f333def17e7835941171299a8f15a97a87
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-29 15:14:34 +0200

    Fix rubocop errors

--- lib/bot.rb
+++ lib/bot.rb
@@ -17,11 +17,11 @@ class Bot
         when 'quote'
           quote_obj = LookUp.new('https://programming-quotes-api.herokuapp.com/quotes/lang/en')
           my_quote = quote_obj.read
-          bot.api.send_message( chat_id: message.chat.id, text: "#{Formatter.format_response('quote',my_quote)}")
+          bot.api.send_message(chat_id: message.chat.id, text: Formatter.format_response('quote', my_quote).to_s)
         when 'word'
           quote_obj = LookUp.new('https://type.fit/api/quotes')
           my_word = quote_obj.read
-          bot.api.send_message(chat_id: message.chat.id, text: "#{Formatter.format_response('word',my_word)}")
+          bot.api.send_message(chat_id: message.chat.id, text: Formatter.format_response('word', my_word).to_s)
         when 'time'
           current_date_time = Time.new
           bot.api.send_message(chat_id: message.chat.id, text: "Hello #{message.from.first_name} #{message.from.last_name},\nThe current time is #{current_date_time.strftime('%I:%M %p')}")
--- lib/formatter.rb
+++ lib/formatter.rb
@@ -1,10 +1,10 @@
 class Formatter
-    def self.format_response(res_type, res_object)
-        case res_type
-        when 'quote'
-            return "Today\'s Programming quote is:\n\n#{res_object['en']}\n\nAuthor:\t#{res_object['author']}"
-        when 'word'
-            return "Today\'s word of bravity is:\n\n#{res_object['text']}\n\nAuthor:\t#{res_object['author']}"
-        end
+  def self.format_response(res_type, res_object)
+    case res_type
+    when 'quote'
+      "Today\'s Programming quote is:\n\n#{res_object['en']}\n\nAuthor:\t#{res_object['author']}"
+    when 'word'
+      "Today\'s word of bravity is:\n\n#{res_object['text']}\n\nAuthor:\t#{res_object['author']}"
     end
+  end
 end
